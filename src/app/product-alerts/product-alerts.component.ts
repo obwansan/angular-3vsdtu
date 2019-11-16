8dfe0bff517357da4dc78679146c88d14befa4c9
obwansan <kob123@hotmail.co.uk>
2019-11-16 21:57:24 +0000

    3 files modified, 1 added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
+  // The @Input() decorator indicates that the property value passes in from the component's parent, the product list component. How? 'products' is imported into product-list.component.ts. It's therefore available to be looped over in product-list.component.html. Each 'product' in 'products' is accessible via @Input() in this component because the ProductAlertsComponent is the child of the ProductListComponent. What makes it the child? Because  <app-product-alerts> is included in product-list.component.html.
+  @Input() product;
 
   constructor() { }
 
