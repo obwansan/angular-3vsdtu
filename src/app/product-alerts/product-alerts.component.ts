609dca77e28187f839f01ec83af6313cf764729b
obwansan <kob123@hotmail.co.uk>
2019-11-17 11:49:54 +0000

    First app - components and template syntax

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
@@ -9,6 +10,9 @@ import { Input } from '@angular/core';
 export class ProductAlertsComponent implements OnInit {
   // The @Input() decorator indicates that the property value passes in from the component's parent, the product list component. How? 'products' is imported into product-list.component.ts. It's therefore available to be looped over in product-list.component.html. Each 'product' in 'products' is accessible via @Input() in this component because the ProductAlertsComponent is the child of the ProductListComponent. What makes it the child? Because  <app-product-alerts> is included in product-list.component.html.
   @Input() product;
+
+  // This allows the product alert component to emit an event when the value of the notify property changes. The @Output decorator passes data to the component's parent component (in this case ProductAlertsComponent).
+  @Output() notify = new EventEmitter();
 
   constructor() { }
 
