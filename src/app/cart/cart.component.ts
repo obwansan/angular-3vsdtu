ca26e5ebba17b7a2a06c4ee2fd02ba4f5b200c59
obwansan <kob123@hotmail.co.uk>
2019-11-18 13:06:57 +0000

    Forms

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -8,11 +9,30 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit {
   items;
+  checkoutForm;
 
-  constructor(private cartService: CartService) { }
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+    ) { 
+
+    }
 
   ngOnInit() {
     this.items = this.cartService.getItems();
+    // During checkout, the app will prompt the user for a name and address. So that you can gather that information later, set the checkoutForm property with a form model containing name and address fields, using the FormBuilder#group() method.
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  }
+
+  onSubmit(customerData) {
+    // Process checkout data here
+    console.warn('Your order has been submitted', customerData);
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
   }
 
 }
